8add6ba73ddda95c67f1b93055769284bf3c694b
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2024-12-02 19:41:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
   // write your code here
-}
+	let result = "";
+	let charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+	for(let i = 0; i < l; i++)
+		{
+			result += charList.charAt(Math.floor(Math.random()*charList.length));
+		}
+	return result;
+	
+}
+ 
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
